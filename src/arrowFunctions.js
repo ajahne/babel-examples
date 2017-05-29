dce7249c43cdeae8840491ed03d95ac57d25e2e0
ajahne <ajahne@gmail.com>
2017-05-28 23:26:57 -0400

    initial commit of arrow function examples

---
+++ src/arrowFunctions.js
@@ -0,0 +1,13 @@
+var add = function(x,y) {
+  return x + y;
+}
+
+var addFat = (x,y) => x + y;
+
+var return12 = () => 12;
+
+var multiply = () => {
+  var x = 5;
+  var y = 10;
+  return x * y;
+};
