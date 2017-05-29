51bd15293b4729944fc8fa89d108a385d3d19381
ajahne <ajahne@gmail.com>
2017-05-28 23:00:14 -0400

    added const and a few more let statements

--- src/app.js
+++ src/app.js
@@ -1,2 +1,21 @@
-let message = "Hello World";
+var message = "Hello World";
+{
+  let message = "Hello Block";
+  console.log(message);
+}
 console.log(message);
+
+//for loop with let
+for (let i = 0; i < 5; i++) {
+  console.log('logging with let %o', i);
+}
+
+//will trigger an error:
+//console.log('outside of the loop, i is %o', i);
+
+//lets try some contants
+{
+  const a = 2;
+  console.log(a);
+  // a = 10; NOTE: will cause a syntax error
+}
