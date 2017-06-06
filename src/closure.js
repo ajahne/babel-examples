a672a5e70c5ed40bc747d6872249d56453f850a9
ajahne <ajahne@gmail.com>
2017-06-05 23:53:40 -0400

    added privacy example

--- src/closure.js
+++ src/closure.js
@@ -3,4 +3,11 @@ const addAreaCode = (areaCode) => (number) => (areaCode + number);
 const addSPringfieldAreaCode = addAreaCode('413-');
 console.log(addSPringfieldAreaCode('212-7743'));
 
-//privacy - TBD
+//privacy
+var counter = () => {
+  var value = 0;
+  var increment = () => {value++};
+  return {
+    increment
+  }
+}
