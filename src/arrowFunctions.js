61c34c41967013816583410f5fdaf7b1a0dd30c0
ajahne <ajahne@gmail.com>
2017-07-12 23:03:12 -0400

    changed "var" to "let"

--- src/arrowFunctions.js
+++ src/arrowFunctions.js
@@ -1,17 +1,17 @@
-var add = function(x,y) {
+let add = function(x,y) {
   return x + y;
 }
 
-var addFat = (x,y) => x + y;
+let addFat = (x,y) => x + y;
 
-var return12 = () => 12;
+let return12 = () => 12;
 
-var multiply = () => {
-  var x = 5;
-  var y = 10;
+let multiply = () => {
+  let x = 5;
+  let y = 10;
   return x * y;
 };
 
-var sayName = (name) => {console.log(name);}
+let sayName = (name) => {console.log(name);}
 
 sayName('foo');
