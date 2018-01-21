bca6224ab49bc35496a49ed9128878ef983ffadd
ajahne <ajahne@gmail.com>
2018-01-20 22:51:04 -0500

    initial commit

---
+++ src/module/app.js
@@ -0,0 +1,5 @@
+//use the module
+import {add} from './module.js';
+import multiply from './module.js';
+console.log(add(5,4));
+console.log(multiply(5,4));
---
+++ src/module/module.js
@@ -0,0 +1,7 @@
+export function add(x,y) {
+  return x + y;
+}
+
+let multiply = (x,y) => x * y;
+
+export default multiply;
