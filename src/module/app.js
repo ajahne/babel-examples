b6429c3a07c44c5a9dd6cad2a27390f03745ac97
ajahne <ajahne@gmail.com>
2018-01-23 23:19:16 -0500

    latest

--- src/module/app.js
+++ src/module/app.js
@@ -1,5 +1,26 @@
-//use the module
-import {add} from './module.js';
-import multiply from './module.js';
-console.log(add(5,4));
-console.log(multiply(5,4));
+// //use the module
+// import {add, log} from './module.js';
+// import {multiply} from './module.js';
+// import foo from './module.js';
+//
+// console.log(add(5,4));
+// console.log(multiply(5,4));
+// log('Hello World');
+
+// import log from './mathUtils.js';
+// log('hello world');
+
+
+// import {add, subtract} from './mathUtils.js';
+// console.log(add(5,4));
+// console.log(subtract(5,4));
+
+
+// import * as mathUtils from './mathUtils.js';
+// console.log(mathUtils.multiply(5,5));  //55
+
+// import multiply from './mathUtils.js';
+// log('Hello World'); //Hello World
+
+import {multiply} from './mathUtils.js';
+console.log(multiply(5,5)); //Hello World
