1356bf579d41999416736a3498515f454f2c2144
ajahne <ajahne@gmail.com>
2018-01-23 22:16:54 -0500

    initial commit

---
+++ src/module/mathUtils.js
@@ -0,0 +1,22 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function divide(a,b) {
+  return a / b;
+}
+
+function logAnswer(answer) {
+  console.log(answer);
+}
+
+export default logAnswer;
+export {add,subtract,multiply,divide};
